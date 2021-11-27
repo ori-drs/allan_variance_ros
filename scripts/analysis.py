768efcc61d263c36278eb82cd07b2a1297566bd0
Martin Günther <martin.guenther@dfki.de>
2021-11-27 10:42:30 +0100

    Fix units again
    I made a mistake in #4 (ee29d7b5). The tables I posted there were
    correct, but my code changes did not reflect what was in the tables.
    
    Thanks @narutojxl for spotting this! (https://github.com/ori-drs/allan_variance_ros/issues/2#issuecomment-980492911).

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -109,9 +109,9 @@ print(f"X Bias Instability: {accel_min_x: .5f} m/s^2 {accel_min_x*3600*3600: .5f
 print(f"Y Bias Instability: {accel_min_y: .5f} m/s^2 {accel_min_y*3600*3600: .5f} m/hr^2")
 print(f"Z Bias Instability: {accel_min_z: .5f} m/s^2 {accel_min_z*3600*3600: .5f} m/hr^2")
 
-print(f"X Accel Random Walk: {accel_rr_intercept_x: .5f} m/s^3/sqrt(s)")
-print(f"Y Accel Random Walk: {accel_rr_intercept_y: .5f} m/s^3/sqrt(s)")
-print(f"Z Accel Random Walk: {accel_rr_intercept_z: .5f} m/s^3/sqrt(s)")
+print(f"X Accel Random Walk: {accel_rr_intercept_x: .5f} m/s^2/sqrt(s)")
+print(f"Y Accel Random Walk: {accel_rr_intercept_y: .5f} m/s^2/sqrt(s)")
+print(f"Z Accel Random Walk: {accel_rr_intercept_z: .5f} m/s^2/sqrt(s)")
 
 average_vrw = (accel_wn_intercept_x + accel_wn_intercept_y + accel_wn_intercept_z) / 3
 average_abi = (accel_rr_intercept_x + accel_rr_intercept_y + accel_rr_intercept_z) / 3
@@ -189,9 +189,9 @@ print(f"X Bias Instability: {gyro_min_x: .5f} deg/s {gyro_min_x*60*60: .5f} deg/
 print(f"Y Bias Instability: {gyro_min_y: .5f} deg/s {gyro_min_y*60*60: .5f} deg/hr")
 print(f"Z Bias Instability: {gyro_min_z: .5f} deg/s {gyro_min_z*60*60: .5f} deg/hr")
 
-print(f"X Rate Random Walk: {gyro_rr_intercept_x: .5f} deg/s^2/sqrt(s)")
-print(f"Y Rate Random Walk: {gyro_rr_intercept_y: .5f} deg/s^2/sqrt(s)")
-print(f"Z Rate Random Walk: {gyro_rr_intercept_z: .5f} deg/s^2/sqrt(s)")
+print(f"X Rate Random Walk: {gyro_rr_intercept_x: .5f} deg/s/sqrt(s)")
+print(f"Y Rate Random Walk: {gyro_rr_intercept_y: .5f} deg/s/sqrt(s)")
+print(f"Z Rate Random Walk: {gyro_rr_intercept_z: .5f} deg/s/sqrt(s)")
 
 average_arw = (gyro_wn_intercept_x + gyro_wn_intercept_y + gyro_wn_intercept_z) / 3
 average_abi = (gyro_rr_intercept_x + gyro_rr_intercept_y + gyro_rr_intercept_z) / 3
